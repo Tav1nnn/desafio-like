0840aa46cc12f9a1d876d3d9cbcdb3b61b747adf
Otavio Marques <marquesotavio251204@gmail.com>
2024-01-26 19:21:12 -0300

    first commit

---
+++ src/main/java/com/otavio/desafiolike/DesafioLikeApplication.java
@@ -0,0 +1,13 @@
+package com.otavio.desafiolike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otavio/desafiolike/DesafioLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otavio.desafiolike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
